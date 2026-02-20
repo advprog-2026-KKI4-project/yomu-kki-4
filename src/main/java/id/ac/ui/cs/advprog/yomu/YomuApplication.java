721307f4f27f4778b1789fb1976b478cade298c8
mvfi0 <muhammad.vegard@ui.ac.id>
2026-02-20 19:37:29 +0700

    Initial code

---
+++ src/main/java/id/ac/ui/cs/advprog/yomu/YomuApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YomuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YomuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/yomu/YomuApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YomuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
