36e3c14a8eb1861c612580213272a167e5c63322
unknown <playarisa24@gmail.com>
2026-03-06 19:22:32 +0700

    feat: add base repository for clan and clan-member

---
+++ src/main/java/id/ac/ui/cs/advprog/yomu/repository/ClanMemberRepository.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.yomu.repository;
+
+import id.ac.ui.cs.advprog.yomu.model.ClanMember;
+import id.ac.ui.cs.advprog.yomu.model.Clan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ClanMemberRepository extends JpaRepository<ClanMember, Long> {
+    List<ClanMember> findByClanId(Clan clan);
+    Optional<ClanMember> findByStudentId(String studentId);
+}
