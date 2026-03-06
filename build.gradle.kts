d817825dfb82bc97fb97a66468ca5c65fd1f4081
mvfi0 <muhammad.vegard@ui.ac.id>
2026-03-06 17:20:45 +0700

    Configure build.gradle.kts and set up PostgreSQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,10 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.h2database:h2")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
