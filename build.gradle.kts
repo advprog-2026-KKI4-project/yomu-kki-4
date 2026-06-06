d25fe9b69f3ab1c78e920c35f98709d0a87007f5
Herdayani Elision Sitio <herdayanisitio@Herdayanis-MacBook-Pro.local>
2026-06-06 22:47:53 +0700

    jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    jacoco
     id("org.springframework.boot") version "4.0.3"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -14,6 +15,9 @@ java {
     }
 }
 
+jacoco {
+    toolVersion = "0.8.11"
+}
 
 configurations {
     compileOnly {
@@ -70,3 +74,27 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    dependsOn(tasks.jacocoTestReport)
+
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.90".toBigDecimal()
+            }
+        }
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
+}
